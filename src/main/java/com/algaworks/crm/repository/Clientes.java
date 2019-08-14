982a819752a6addc04e596c1989e157139c9d87d
Alexandre Afonso <alexandre.afonso@algaworks.com>
2019-08-14 20:04:01 -0300

    Comentário adicionado durante a live.

--- src/main/java/com/algaworks/crm/repository/Clientes.java
+++ src/main/java/com/algaworks/crm/repository/Clientes.java
@@ -7,8 +7,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.algaworks.crm.model.Cliente;
 
 public interface Clientes extends JpaRepository<Cliente, Long> {
-	
+
+	// select c from Cliente where c.id = :id and c.tenant = :tenant
 	Cliente findByIdAndTenant(Long id, String tenant);
-	
+
+	// select c from Cliente where c.tenant = :tenant
 	List<Cliente> findAllByTenant(String tenant);
 }
